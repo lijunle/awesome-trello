90900c2e938cb77942dc6b471e14075f12654974
Junle Li <lijunle@gmail.com>
2016-08-07 04:28:02 +0800

    Minify the JavaScript for deployment.
    - Extract the ELM compile script as a node.js script.
    - Update the Travis CI config to run it.

---
+++ Awesome.Trello/elm-build.js
@@ -0,0 +1,9 @@
+var run = require('../build/run');
+
+var commands = [
+  'elm package install --yes',
+  'elm make index.elm --output obj/index.js --warn',
+  'uglifyjs obj/index.js --compress warnings=false --mangle --stats --output index.js'
+];
+
+run(__dirname, commands);
