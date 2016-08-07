842fa3e6280a3c08377e5bb7a2c0c17a068273d4
Junle Li <lijunle@gmail.com>
2016-08-07 16:43:49 +0800

    Extract the login logic to a separated module.
    - Continue to use index.elm as entry point.
    - Leverage Logic module in entry point.

--- Awesome.Trello/index.elm
+++ Awesome.Trello/index.elm
@@ -2,11 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.App as App
-import Html.Attributes exposing (..)
-import Http
-import Json.Decode as Json
-import Json.Decode exposing ((:=))
-import Task
+import Login
 
 
 main : Program Never
@@ -19,92 +15,45 @@ main =
         }
 
 
-type alias Name =
-    String
-
-
-type alias Url =
-    String
-
-
-type alias Config =
-    { name : Maybe Name
-    }
-
-
-type Model
-    = Init
-    | Login
-    | Logout Name
+type alias Model =
+    { login : Login.Model }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Init, getConfig )
+    let
+        ( loginModel, loginMsg ) =
+            Login.init
+    in
+        ( Model loginModel, Cmd.map LoginMsg loginMsg )
 
 
 type Msg
-    = GetConfig
-    | FetchSucceed Config
-    | FetchFail Http.Error
+    = Init
+    | LoginMsg Login.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GetConfig ->
-            ( model, getConfig )
-
-        FetchSucceed config ->
-            ( config |> toModel, Cmd.none )
-
-        FetchFail error ->
+        Init ->
             ( model, Cmd.none )
+
+        LoginMsg msg ->
+            let
+                ( loginModel, loginMsg ) =
+                    Login.update msg model.login
+            in
+                ( Model loginModel, Cmd.map LoginMsg loginMsg )
 
 
 view : Model -> Html Msg
 view model =
-    case model of
-        Init ->
-            text ""
-
-        Login ->
-            a [ href "/login" ] [ text "Login" ]
-
-        Logout name ->
-            div []
-                [ text "Hi, "
-                , text name
-                , a [ href "/logout" ] [ text "Logout" ]
-                ]
+    div []
+        [ App.map LoginMsg (Login.view model.login)
+        ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
-
-getConfig : Cmd Msg
-getConfig =
-    let
-        url =
-            "/config.json"
-    in
-        Task.perform FetchFail FetchSucceed (Http.get decodeUrl url)
-
-
-toModel : Config -> Model
-toModel config =
-    case config.name of
-        Just name ->
-            Logout name
-
-        Nothing ->
-            Login
-
-
-decodeUrl : Json.Decoder Config
-decodeUrl =
-    Json.object1
-        Config
-        ("Name" := (Json.maybe Json.string))
---
+++ Awesome.Trello/login.elm
@@ -0,0 +1,94 @@
+module Login exposing (Model, Msg, init, update, view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Http
+import Json.Decode as Json
+import Json.Decode exposing ((:=))
+import Task
+
+
+type alias Name =
+    String
+
+
+type alias Url =
+    String
+
+
+type alias Config =
+    { name : Maybe Name
+    }
+
+
+type Model
+    = Init
+    | Login
+    | Logout Name
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Init, getConfig )
+
+
+type Msg
+    = GetConfig
+    | FetchSucceed Config
+    | FetchFail Http.Error
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetConfig ->
+            ( model, getConfig )
+
+        FetchSucceed config ->
+            ( config |> toModel, Cmd.none )
+
+        FetchFail error ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Init ->
+            text ""
+
+        Login ->
+            a [ href "/login" ] [ text "Login" ]
+
+        Logout name ->
+            div []
+                [ text "Hi, "
+                , text name
+                , a [ href "/logout" ] [ text "Logout" ]
+                ]
+
+
+getConfig : Cmd Msg
+getConfig =
+    let
+        url =
+            "/config.json"
+    in
+        Task.perform FetchFail FetchSucceed (Http.get decodeUrl url)
+
+
+toModel : Config -> Model
+toModel config =
+    case config.name of
+        Just name ->
+            Logout name
+
+        Nothing ->
+            Login
+
+
+decodeUrl : Json.Decoder Config
+decodeUrl =
+    Json.object1
+        Config
+        ("Name" := (Json.maybe Json.string))
