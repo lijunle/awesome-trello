0a63efd9f64f26fb0e7173c305cf2bd409f4ba12
Junle Li <lijunle@gmail.com>
2016-08-08 12:25:44 +0800

    Fix the elm-build script on Windows.
    - Windows use `Path` env variable instead of `PATH`.

--- build/run.js
+++ build/run.js
@@ -4,13 +4,15 @@ var execSync = require('child_process').execSync;
 
 var bin = path.resolve(__dirname, '../node_modules/.bin');
 var envPath = bin + path.delimiter + process.env.PATH;
+var envBag = process.platform === 'win32' ? { Path: envPath } : { PATH: envPath };
+var env = extend({}, process.env, envBag);
 
 module.exports = function run(cmd, commands) {
   commands.forEach(function (command) {
     console.log('> ' + command);
     execSync(command, {
       cwd: cmd,
-      env: extend({}, process.env, { PATH: envPath }),
+      env: env,
       stdio: ['inherit', 'inherit', 'inherit'],
     });
   });
