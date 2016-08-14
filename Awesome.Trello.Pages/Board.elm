31cba0e08af4bb6d53389b17bff2e911187c0a05
Junle Li <lijunle@gmail.com>
2016-08-15 00:05:28 +0800

    Add selected board and member to model.
    - Render member `id` and `fullName` to the page.
    - Use `update` function to map `id` back to member object.

--- Awesome.Trello.Pages/Board.elm
+++ Awesome.Trello.Pages/Board.elm
@@ -1,6 +1,7 @@
 module Board exposing (Model, Msg, init, update, view)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Decode
@@ -12,6 +13,8 @@ type alias Model =
     { boards : List Board
     , members : List Member
     , cards : List Card
+    , selectedBoard : Maybe Board
+    , selectedMember : Maybe Member
     }
 
 
@@ -19,7 +22,8 @@ type Msg
     = FetchFail Http.Error
     | FetchCardSucceed (List Card)
     | FetchMemberSucceed (List Member)
-    | Select Board
+    | SelectBoard Board
+    | SelectMember String
 
 
 init : Config -> ( Model, Cmd Msg )
@@ -63,13 +67,32 @@ update msg model =
 
         FetchMemberSucceed memberList ->
             let
+                member =
+                    List.head memberList
+
                 newModel =
-                    { model | members = memberList }
+                    { model | members = memberList, selectedMember = member }
             in
                 ( newModel, Cmd.none )
 
-        Select board ->
-            ( model, getCard board )
+        SelectBoard board ->
+            let
+                newModel =
+                    { model | selectedBoard = Just board }
+            in
+                ( newModel, getCard board )
+
+        SelectMember memberId ->
+            let
+                member =
+                    model.members
+                        |> List.filter (\x -> x.id == memberId)
+                        |> List.head
+
+                newModel =
+                    { model | selectedMember = member }
+            in
+                ( newModel, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -88,14 +111,14 @@ view model =
 
 viewBoardSelector : List Board -> Html Msg
 viewBoardSelector boards =
-    select [ onInput Select ]
+    select [ onInput SelectBoard ]
         (boards |> List.map (\x -> option [] [ text x ]))
 
 
 viewMemberSelector : List Member -> Html Msg
 viewMemberSelector members =
-    select []
-        (members |> List.map (\x -> option [] [ text x.fullName ]))
+    select [ onInput SelectMember ]
+        (members |> List.map (\x -> option [ value x.id ] [ text x.fullName ]))
 
 
 viewCardList : List Card -> Html Msg
@@ -111,7 +134,12 @@ viewCardList cards =
 
 toModel : Config -> Model
 toModel config =
-    Model config.boards [] []
+    Model
+        config.boards
+        []
+        []
+        (List.head config.boards)
+        Nothing
 
 
 getCard : Board -> Cmd Msg
