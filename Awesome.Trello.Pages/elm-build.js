8e33bf1df32b1bab1db0bcb094a1078b444e929e
Junle Li <lijunle@gmail.com>
2016-08-29 01:07:19 +0800

    Build everything under Pages folder.
    - Update the Elm build script to generate `index.js` under dist folder.
    - Move the `index.html` from code behind folder to Pages folder.
    - Update git ignore file to ignore dist folder.

--- Awesome.Trello.Pages/elm-build.js
+++ Awesome.Trello.Pages/elm-build.js
@@ -3,10 +3,10 @@ var patch = require('../build/patch');
 
 var commands = [
   'elm package install --yes',
-  'elm make Index.elm --output temp/index.js --warn',
-  'uglifyjs temp/index.js --compress warnings=false --mangle --stats --output ../Awesome.Trello/index.js'
+  'elm make Index.elm --output dist/index.debug.js --warn',
+  'uglifyjs dist/index.debug.js --compress warnings=false --mangle --stats --output ./dist/index.js'
 ];
 
 run(__dirname, commands);
-patch(__dirname, '../Awesome.Trello/index.js', '{TRELLO_KEY}', process.env.TRELLO_KEY);
-patch(__dirname, '../Awesome.Trello/index.js', '{TRELLO_APP_NAME}', process.env.TRELLO_APP_NAME);
+patch(__dirname, './dist/index.js', '{TRELLO_KEY}', process.env.TRELLO_KEY);
+patch(__dirname, './dist/index.js', '{TRELLO_APP_NAME}', process.env.TRELLO_APP_NAME);
